a3f59a2f87f05b86c001e79d310633e5170b4dc0
NaCl-5844 <114276082+NaCl-5844@users.noreply.github.com>
2022-10-17 09:45:35 +0100

    v0.1
    + Placed initial commit into get_t16() - extracts the T16 ISA
    + Beginnings of cross-referencing the input assembly with T16

--- asm.py
+++ asm.py
@@ -3,17 +3,32 @@ import re
 
 script, asm_file, bin_txt = argv
 
-with open(asm_file) as asm:
-	asmDict = {}
-	for i in asm:
-		j = i.split(' ', 1)
-		if len(j) == 2:
-			asmDict[j[0]] = j[1].replace('\n', '')
-print(asmDict)
+_t16_specs_ = {
+	'r': list(range(0, 8)),
+	'e': list(range(0, 8)),
+	'v': list(range(0, 8)),
+}
 
 
 
 
+def get_t16():
+	with open('_t16_format_v3.4_') as t:
+		t16 = {}
+		for op in t:
+			j = op.split(' ', 1)
+			if len(j) == 2:
+				t16[j[0]] = j[1].replace('\n', '')
+	return(t16)
+
+def crossref_asm(asm, bcode):
+	_t16_ = get_t16()
+	with open(asm) as a:
+		for line in a:
+			command = line.split(' ')
+			
+
+print(_t16_specs_['r'])
 
 
 
